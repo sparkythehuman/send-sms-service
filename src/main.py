025ca4f4d1343fbbd85593f1a46c4a3a3da96e18
Joshua T Garcia <garcia.sparky@gmail.com>
2020-11-11 16:40:37 -0700

    wip -- i cannot send the file via json, processing it on the front end

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import boto3
 import csv
+import json
 import os
 from boto3.dynamodb.conditions import And, Attr, Key
 from uuid import uuid4
@@ -9,12 +10,16 @@ from pytz import timezone
 
 contact = boto3.resource('dynamodb').Table('contact')
 
-def _get_username(context):
-    return context['authorizer']['claims']['cognito:username']
+def _get_username(event):
+    return event['requestContext']['authorizer']['claims']['cognito:username']
 
 
 def _get_phone_numbers_from_file(file):
     for row in csv.DictReader(file.read().decode('utf-8-sig').split("\n")):
+        yield row
+
+def _get_phone_numbers(request):
+    for row in request['contacts']:
         yield row
 
 
@@ -38,51 +43,60 @@ def _find(username, **kwargs):
 def _response(phone_numbers):
     return {
         'statusCode': 200,
-        'phone_numbers': phone_numbers
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        'body': json.dumps(phone_numbers)
     }
 
 
-def _get(event, context):
+def _get(request, username):
     phone_numbers = _find(
-        username=_get_username(context),
-        contact_list_id=event['contact-list-id']
+        username=username,
+        contact_list_id=request['contact_list_id']
     )
 
     return _response(phone_numbers)
 
 
-def _create(event, context):
+def _create(request, username):
     # first delete all contacts from the contact_list
     for _contact in _find(
-        username=_get_username(context),
-        contact_list_id=event['contact-list-id']
+        username=username,
+        contact_list_id=request['contact_list_id']
     ):
-        contact.delete_item(Key=_contact['id'])
+        contact.delete_item(
+            Key={'id': _contact['id']}
+        )
 
     # then add the new phone numbers to to the contact_list
-    for _contact in _get_phone_numbers_from_file(event['phone-numbers-csv']):
+    print(request)
+    # for _contact in _get_phone_numbers_from_file(request['phone_numbers_csv']):
+    for _contact in _get_phone_numbers(request):
         contact.put_item(Item={
             'id': 'CPH' + str(uuid4().int)[0:16],
             'contact_name': _contact['name'],
             'phone_number': _contact['phone_number'],
-            'contact_list_id': event['contact-list-id'],
-            'username': _get_username(context), 
+            'contact_list_id': request['contact_list_id'],
+            'username': username, 
             'created_at': datetime.now(tz=timezone('America/Denver')).isoformat(),
             'updated_at': datetime.now(tz=timezone('America/Denver')).isoformat(),
         })
         
     # return the new contact_list of phone numbers
-    return _get(event, context) 
+    return _get(request, username) 
 
 
 def handle(event, context):
-    operation = context['httpMethod']
+    operation = event['requestContext']['httpMethod']
     operations = {
         'GET' : _get,
         'POST': _create
     }
     if operation in operations:
-        return operations[operation](event, context)
+        print(event)
+        return operations[operation](json.loads(event['body']), _get_username(event))
     else:
         raise ValueError(f'Unable to run operation for HTTP METHOD: {operation}')
     
